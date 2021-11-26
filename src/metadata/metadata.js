6640332b44961b5a852a10f024a4b88bad0bcf61
Albert Xiao <albert.aurora@gmail.com>
2021-11-26 09:58:07 +0700

    update metadata and index

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -1,13 +1,13 @@
 module.exports = {
-  name: 'William Shakespeare',
-  title: 'Poet, playwright and actor',
+  name: 'Albert Suteja',
+  title: 'Full Stack Developer',
   facts: {
-    'Residence': 'London',
-    'Homepage': `<a href="https://en.wikipedia.org/wiki/William_Shakespeare">
-                  <span class="print">https://en.wikipedia.org/wiki/William_Shakespeare</span>
-                  <span class="screen">wiki</span>
+    'Residence': 'Malang',
+    'Homepage': `<a href="https://albertxiao.github.io/curvit">
+                  <span class="print">https://albertxiao.github.io/curvit</span>
+                  <span class="screen">Web CV</span>
                 </a>`,
-    'Email': '<a href="mailto:wil64@yahoo.com">wil64@yahoo.com</a>'
+    'Email': '<a href="mailto:albert@ftje.net">albert@ftje.net</a>'
   },
   skills: [
     ['Comedy', '++++'],
@@ -25,44 +25,71 @@ module.exports = {
   ],
   plays: [
     {
-      title: 'Hamlet',
-      written: 'Likely early 17th century',
-      badges: ['tragedy', 'philosophy', 'death'],
-      contents: `
-Prince Hamlet is visited by his father's ghost and ordered to avenge his father's murder by killing King Claudius, his uncle. After struggling with several questions, including whether what the ghost said is true and whether it is right for him to take revenge, Hamlet, along with almost all the other major characters, is killed.
-`
+      title: 'Parttime Software Developer at Kopoka',
+      written: 'Aug 2020 - Present',
+      badges: ['ASP.NET MVC', 'Entity Framework', 'jQuery'],
+      contents: `Develop & Fix Bug at Kopoka.`
     },
     {
-      title: 'Twelfth Night',
-      written: '1600 – 1601',
-      badges: ['comedy', 'love'],
-      contents: `
-Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
-`
+      title: 'Parttime Software Developer at Geekgarden',
+      written: 'Des 2019 - Sep 2020',
+      badges: ['ASP.NET MVC', 'Entity Framework', 'Kendo for jQuery', 'jQuery'],
+      contents: `Develop BRI Survey Management System.`
     },
     {
-      title: 'The Tragedy of Romeo and Juliet',
-      written: '1595–1596, with a possible early draft written in 1591',
-      badges: ['tragedy', 'death', 'love'],
-      contents: `
-In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
-`
+      title: 'Senior Software Engineer for Bizzow',
+      written: 'Aug 2017 - Oct 2019',
+      badges: ['ASP.NET MVC', 'Entity Framework', 'Kendo for jQuery', 'jQuery', 'Azure'],
+      contents: `Develop job & scheduling web applicationg and GPS Device to backend communication.`
+    },
+    {
+      title: 'Analyst & Programmer for Kawan Lama Corporate',
+      written: 'Nov 2012 – Aug 2014',
+      badges: ['C#', 'ASP.NET Web Forms', 'SQL Server', 'jQuery', 'WebServices', 'AJAXControlToolkit', 'Windows Form'],
+      contents: `Develop web app for Human Capital and Employee's Uniform Management`
+    },
+    {
+      title: 'Programmer for PT Rice Bowl Indonesia',
+      written: 'Apr 2012 - Sep 2012',
+      badges: ['C#', 'VB.NET', 'ASP.NET Web Forms', 'SQL Server', 'jQuery', 'WebServices', 'AJAXControlToolkit', 'Windows Form'],
+      contents: `Develop ERP Application & DB Admin`
     }
   ],
   experience: [
     {
-      header: 'Performance',
-      skills: ['theater', 'play'],
-      contents: `
-Thousands of performances of my plays have been staged since the end of the 16th century. Many of my greatest plays were performed by the Lord Chamberlain's Men and King's Men acting companies at the [Globe](https://en.wikipedia.org/wiki/Globe_Theatre) and Blackfriars Theatres.
-`
+      header: 'Freelance Software Developer',
+      skills: ['Android', 'Flutter'],
+      contents: `Develop app to help register user who want to register new company or institution`
     },
     {
-      header: 'Poetry',
-      skills: ['poetry', 'erotic'],
-      contents: `
-In 1593 and 1594, when the theatres were closed because of plague, I published two narrative poems on sexual themes, Venus and Adonis and The Rape of Lucrece. He dedicated them to Henry Wriothesley, Earl of Southampton.
-`
+      header: 'Freelance Software Developer',
+      skills: ['PHP', 'javascript'],
+      contents: `Develop web application to generate financial reports (Journal, Profit & Loss Report, Ledger) for hotel`
+    },
+    {
+      header: 'Freelance Software Developer',
+      skills: ['PHP', 'docker', 'shell script'],
+      contents: `Fix aniworks & animenz website`
+    },
+    {
+      header: 'Freelance Software Developer',
+      skills: ['C#', 'Windows Form', 'SQLite', 'Entity Framework 6'],
+      contents: `Develop application to process data export from SAP to make the format compatible for upload to BPOM website.`
+    },
+    {
+      header: 'Freelance Software Developer',
+      skills: ['javascript'],
+      contents: `Develop script to automate file upload to IDI Website`
+    },
+    {
+      header: 'Home Automation',
+      skills: ['Arduino', 'Electronic Engineering', 'Raspberry Pi'],
+      contents: `Self project to automate house chores & gardening`
+    },
+    {
+      header: 'Final Project',
+      skills: ['C#', 'ASP.NET MVC 3', 'SQL Server', 'jQuery'],
+      contents: `Develop Information System to manage laboratory assets`
     }
   ]
 };
