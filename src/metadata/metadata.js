59ecf87ef401dcdcd4ba4f05a68b0dad9fed4683
Albert Xiao <albert.aurora@gmail.com>
2025-03-08 09:26:36 +0700

    test update contents with new line

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -25,40 +25,43 @@ module.exports = {
   ],
   plays: [
     {
-      title: 'Senior FullStack Engineer',
+      title: 'Senior FullStack Engineer at Cryowerx',
       written: 'Mar 2020 - Present',
       badges: ['NodeJS', 'Golang', 'Java', 'Swift', 'C++', 'Javascript', 'Angular', 'Vue 3', 'Microservices', 'GCP'],
-      contents: `Handle almost anything related to software development, but sometime doing hardware stuff too.`
+      contents: `- Spearheaded the development of IoT-enabled smart fridge applications, improving operational efficiency and customer experience for automated retail.
+- Rewrote the frontend to utilize Vue 3, ensuring smoother user experience and easier maintainability for future feature updates.
+- Increased overall backend stability and improved robustness, reducing crashes due to errors to 0%.
+- Improved mobile app robustness, significantly decreasing crashes, with 98% of sessions showing no crashes in Firebase Crashlytics reports.
+- Designed and deployed microservices in Docker across AWS, DigitalOcean, and GCP, enhancing system resilience and achieving 99.9% uptime.
+`
     },
     {
       title: 'Parttime Software Developer at Geekgarden',
       written: 'Sep 2019 - Jan 2020',
       badges: ['ASP.NET MVC', 'Entity Framework', 'Kendo for jQuery', 'jQuery', 'Microservices'],
-      contents: `Develop BRI Survey Management System.`
+      contents: `Develop BRI Bank's Survey Management System.`
     },
     {
       title: 'Senior Software Engineer for Bizzow',
       written: 'Aug 2017 - Oct 2019',
       badges: ['ASP.NET MVC', 'Entity Framework', 'Kendo for jQuery', 'jQuery', 'Azure'],
-      contents: `Develop job & scheduling web applicationg and GPS Device to backend communication.`
-    },
-    {
-      title: 'Do Freelancing Works + Repair Shop Owner',
-      written: 'Jan 2015 - Jul 2019',
-      badges: ['ASP.NET MVC', 'Entity Framework', 'PHP', 'jQuery', 'Azure', 'AWS', 'NodeJS'],
-      contents: `Married, and move to Malang.`
+      contents: `- Developed a GPS device communication system integrated with a job and scheduling application, improving field technician efficiency by 25%.
+- Utilized ASP.NET MVC and Azure to build scalable backend systems
+`
     },
     {
       title: 'Analyst & Programmer for Kawan Lama Corporate',
       written: 'Nov 2012 – Aug 2014',
       badges: ['C#', 'ASP.NET Web Forms', 'SQL Server', 'jQuery', 'WebServices', 'AJAXControlToolkit', 'Windows Form'],
-      contents: `Develop web app for Human Capital and Employee's Uniform Management`
+      contents: `- Developed and launched a comprehensive Human Capital Information System, improving HR workflows and reducing manual processing time.
+- Delivered an Employee Uniform Management Information System, automating the uniform issuance process, saving hours needed to manage provisions for 6000+ employee.`
     },
     {
       title: 'Programmer for PT Rice Bowl Indonesia',
       written: 'Apr 2012 - Sep 2012',
       badges: ['C#', 'VB.NET', 'ASP.NET Web Forms', 'SQL Server', 'jQuery', 'WebServices', 'AJAXControlToolkit', 'Windows Form'],
-      contents: `Develop ERP Application & DB Admin`
+      contents: `- Developed an ERP system to streamline enterprise resource planning and improve decision-making processes for management.
+- Enhanced absence management and reporting systems, resulting in 20% improved accuracy in attendance tracking.`
     }
   ],
   experience: [
